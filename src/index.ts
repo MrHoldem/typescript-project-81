d855a9c70a24b6484df26968e684b3ca8c6321bf
Mr. Holdem <aleksnadrfurmanoa@gmail.com>
2025-05-11 12:22:49 +0300

    Поменял файловую структуру проекта

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,4 @@
-type Attributes = {
-    [name: string] : string
-}
-
-export class Tag {
-    private static tagsTypes : Map<string, boolean> = new Map<string, boolean>([
-        ["div", true],
-        ["input", false],
-        ["br", false],
-        ["img", false],
-        ["label", true],
-    ]);
-
-    constructor(private tagName: string, private attributes: Attributes = {}, private content : string | null = null) {
-        if (this.content === null)
-            this.content = "";
-    }
-
-    toString() : string {
-        const tagType = Tag.tagsTypes.get(this.tagName);
-        let result : string;
-        result = `<${this.tagName}${this.attributesToString()}>`;
-
-        if (tagType === false) // Игнорируем content, если тег одинарный.
-            return result;
-
-        if (tagType === true || this.content?.length)
-            return result + this.content + `</${this.tagName}>`;
-
-        return result;
-    }
-
-    private attributesToString() : string {
-        if (!Object.entries(this.attributes).length)
-            return "";
-        return " " + Object.entries(this.attributes)
-                .map((value: [string, string]) => `${value[0]}="${value[1]}"`)
-                .join(" ");
-    }
-}
+import Tag from "./Tags";
 
 console.log(new Tag("br").toString());
 // <br>
