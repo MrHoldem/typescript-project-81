bbaf6e020c8af3c1ada7daea6c4ba3068fecc906
Mr. Holdem <aleksnadrfurmanoa@gmail.com>
2025-05-11 11:59:32 +0300

    Реализовал класс Tag

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+type Attributes = {
+    [name: string] : string
+}
+
+export class Tag {
+    private static tagsTypes : Map<string, boolean> = new Map<string, boolean>([
+        ["div", true],
+        ["input", false],
+        ["br", false],
+        ["img", false],
+        ["label", true],
+    ]);
+
+    constructor(private tagName: string, private attributes: Attributes = {}, private content : string | null = null) {
+        if (this.content === null)
+            this.content = "";
+    }
+
+    toString() : string {
+        const tagType = Tag.tagsTypes.get(this.tagName);
+        let result : string;
+        result = `<${this.tagName}${this.attributesToString()}>`;
+
+        if (tagType === false) // Игнорируем content, если тег одинарный.
+            return result;
+
+        if (tagType === true || this.content?.length)
+            return result + this.content + `</${this.tagName}>`;
+
+        return result;
+    }
+
+    private attributesToString() : string {
+        if (!Object.entries(this.attributes).length)
+            return "";
+        return " " + Object.entries(this.attributes)
+                .map((value: [string, string]) => `${value[0]}="${value[1]}"`)
+                .join(" ");
+    }
+}
+
+console.log(new Tag("br").toString());
+// <br>
+
+console.log(new Tag("img", { src: "path/to/image" }).toString());
+// <img src="path/to/image">
+
+console.log(new Tag("input", { type: "submit", value: "Save" }).toString());
+// <input type="submit" value="Save">
+
+// Для парных тегов надо придумать как лучше
+console.log(new Tag("label", {}, "Email").toString());
+// <label>Email</label>
+
+console.log(new Tag("label", { for: "email" }, "Email").toString());
+// <label for="email">Email</label>
+
+console.log(new Tag("div").toString());
+// <div></div>
