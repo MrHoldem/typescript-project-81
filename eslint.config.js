93e6337be763bd17e3f5ecb79fa345d790052429
Mr. Holdem <aleksnadrfurmanoa@gmail.com>
2025-05-16 15:56:13 +0300

    Поправил конфиг линтера, добавил его в пайплайн

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,24 @@
-import eslintPluginTs from '@typescript-eslint/eslint-plugin';
-import parser from '@typescript-eslint/parser';
+  import eslintPluginTs from '@typescript-eslint/eslint-plugin';
+  import parser from '@typescript-eslint/parser';
 
-export default [
-  {
-    files: ['**/*.ts'],
-    languageOptions: {
-      parser,
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
+  export default [
+    {
+      files: ['**/*.ts'],
+      languageOptions: {
+        parser,
+        parserOptions: {
+          ecmaVersion: 2020,
+          sourceType: 'module',
+        },
+      },
+      plugins: {
+        '@typescript-eslint': eslintPluginTs,
+      },
+      rules: {
+        semi: ['error', 'double'],
+        quotes: ['error', 'always'],
+        quotes: 'off',
+        semi: 'off',
       },
     },
-    plugins: {
-      '@typescript-eslint': eslintPluginTs,
-    },
-    rules: {
-      semi: ['error', 'never'],
-      quotes: ['error', 'single'],
-    },
-  },
-]
+  ]
